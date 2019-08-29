75cb3b32536e36a59d6436cca851452cdf56be64
yangjian <412319303@qq.com>
2019-08-29 17:28:22 +0800

    提交2019.08.29练习

---
+++ hui_practice/src/main/java/com/example/hui_practice/HuiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hui_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiPracticeApplication.class, args);
+    }
+
+}
