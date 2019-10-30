24b0394fb798e986df5573f7470d80d1ec584c62
yangjian <yangjian1011>
2019-10-30 23:06:57 +0800

    2019-10-30添加nacos的配置练习

---
+++ nacos_demo/src/main/java/com/example/nacos_demo/NacosDemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.nacos_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YangJian
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDemoApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
