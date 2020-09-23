5c9802e7269837ddc6d9d26ea2e1a734e70640f4
YngJian <820335273@qq.com>
2020-09-23 20:54:29 +0800

    web-flush

--- vayne_practice/src/main/java/com/example/practice/leetcode/Solution.java
+++ vayne_practice/src/main/java/com/example/practice/leetcode/Solution.java
@@ -51,7 +51,23 @@ public class Solution {
         return stringBuilder.toString();
     }
 
+    public static int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
+        int reverse = reverse(123);
+        System.out.println(reverse);
         String convert = Solution.convert("LEETCODEISHIRING", 4);
         System.out.println(convert);
     }
