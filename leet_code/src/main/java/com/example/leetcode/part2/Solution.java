b57e701829627eccd0c449af669df8928697bc42
yangjian <yangjian1011>
2021-06-01 22:11:37 +0800

    feat: day2练习

---
+++ leet_code/src/main/java/com/example/leetcode/part2/Solution.java
@@ -0,0 +1,64 @@
+package com.example.leetcode.part2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。。
+ *
+ * https://blog.csdn.net/weixin_39799565/article/details/112096560 hash复杂度对比
+ * @author : Yang Jian
+ * @date : 2021/5/31 0031 22:31
+ */
+public class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int another = target - nums[i];
+            if (map.containsKey(another)) {
+                return new int[]{map.get(another), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 4};
+        System.out.println(Arrays.toString(twoSum(nums, 6)));
+    }
+}
