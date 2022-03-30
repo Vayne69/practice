b9d1f80663fbe887c28f8fb4f461de5cfdbb35fd
yangjian <820335273@qq.com>
2022-03-30 14:16:02 +0800

    feat:neo4j示例

---
+++ neo4j-practice/src/main/java/com/example/neo4jpractice/Neo4jPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo4jpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jPracticeApplication.class, args);
+    }
+
+}
