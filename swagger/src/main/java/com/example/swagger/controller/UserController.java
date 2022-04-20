e1d039a2fabc9414117facc3ff23adc76f76623f
yangjian <820335273@qq.com>
2022-04-20 14:46:14 +0800

    fix:修改生成

--- swagger/src/main/java/com/example/swagger/controller/UserController.java
+++ swagger/src/main/java/com/example/swagger/controller/UserController.java
@@ -15,25 +15,25 @@ import springfox.documentation.annotations.ApiIgnore;
 @Api(tags = "用户相关接口", description = "提供用户相关的rest api")
 public class UserController {
     @ApiOperation("新增用户接口")
-    @PostMapping("/add")
+    @PostMapping("/")
     public boolean addUser(@RequestBody User user) {
         return false;
     }
 
     @ApiOperation("通过id查找用户接口")
-    @GetMapping("/find/{id}")
+    @GetMapping("/{id}")
     public User findById(@PathVariable("id") int id) {
         return new User();
     }
 
     @ApiOperation("更新用户信息接口")
-    @PutMapping("/update")
-    public boolean update(@RequestBody User user) {
+    @PutMapping("/{id}")
+    public boolean update(@PathVariable("id") int id, @RequestBody User user) {
         return true;
     }
 
     @ApiOperation("删除用户接口")
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     // @ApiIgnore
     public boolean delete(@PathVariable("id") int id) {
         return true;
